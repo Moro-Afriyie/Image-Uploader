723db90847f01628b15403494cba977a08aafdae
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-27 09:59:33 +0000

    configure tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 // eslint-disable-next-line require-jsdoc
 function App() {
-  return <div>Image uploader</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-red-900">Hello world!</h1>
+  );
 }
 
 export default App;
